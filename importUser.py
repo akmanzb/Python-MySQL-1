95e6a1eff511bfa5f0b372e952f5ccd25ab6faad
Bugra.Akman <akmanbugra@gmail.com>
2023-07-20 17:30:29 +0300

    Menü alanı düzenlendi.

--- importUser.py
+++ importUser.py
@@ -22,29 +22,33 @@ def userLogin():
     values = (username,password)
 
     cursor.execute(sql,values)
-    result = cursor.fetchone()
-
-    if result == None:
-        print("You entered your login information incorrectly.! Please try again.\n")
-        userLogin()
-    elif result[1]==username and result[2]==password:
-        menu(username)
+    try:
+        result = cursor.fetchone()
+        if result == None:
+            print("You entered your login information incorrectly.! Please try again.\n")
+            userLogin()
+        elif result[1]==username and result[2]==password:
+            menu(username)
+    except mysql.connector.Error as err:
+        print("error: ", err)
+    finally:
+        connection.close()
 
 def menu(username):
     os.system('clear')
-    print(f"Welcome {username}.\n\nTransaction Options\n-----------------")
-    print("1.| Student List")
-    print("2.| Add Student")
-    print("3.| Edit Student")
-    print("4.| Delete Student")
-    print("5.| Authorized List")
-    print("6.| Add Authorized")
-    print("7.| Edit Authorized")
-    print("8.| Delete Authorized")
-    print("9.| Exit")
+    print(f"Welcome {username}.\n\nTransaction Options\n-------------------")
+    print("1.| Student List\t|")
+    print("2.| Add Student\t\t|")
+    print("3.| Edit Student\t|")
+    print("4.| Delete Student\t|")
+    print("5.| Authorized List\t|")
+    print("6.| Add Authorized\t|")
+    print("7.| Edit Authorized\t|")
+    print("8.| Delete Authorized\t|")
+    print("9.| Exit\t\t|")
     choice = int(input("\nPlease select the transaction number you want to make : "))
 
-def importUser():
+def addUser():
 
     username = input("Username: ")
     password = input("Password: ")
